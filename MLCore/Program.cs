e3facdb8af7c226650ebbabb89c8fd301b41ef30
hafniz <42380178+hafniz@users.noreply.github.com>
2019-04-19 12:08:09 +0800

    Remove no longer needed projects; Set up .net Core 3 / C# 8 project environment

---
+++ MLCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.ML;
+
+namespace MLCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+            Console.WriteLine(mlContext);
+        }
+    }
+}
