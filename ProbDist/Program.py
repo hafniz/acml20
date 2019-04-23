b0cbc29e4bbd6551113e808dcdb43bc14b0349f3
Hafnia Vasilyevna <42380178+hafniz@users.noreply.github.com>
2019-04-23 16:33:36 +0800

    Delete never-working code

--- ProbDist/Program.py
+++ ProbDist/Program.py
@@ -3,15 +3,9 @@ from Instance import Instance
 from KNNContext import KNNContext
 from NaiveBayesContext import NaiveBayesContext
 from DecisionTreeContext import DecisionTreeContext
-"""
+
 digits = datasets.load_digits()
 mlContext = NaiveBayesContext()
 mlContext.SetTrainingData(Instance.ReadFromCSV(".\\data\\glass.data", False, True))
 mlContext.Train()
 print(mlContext.GetProbDist(Instance([1.51937,13.79,2.41,1.19,72.76,0.00,9.77,0.00,0.00])))
-"""
-
-from weka.classifiers import IBk
-c = IBk(K=1)
-c.train("C:\\Users\\CHENH\\Anaconda3\\Lib\\site-packages\\weka\\fixtures\\abalone-train.arff")
-predictions = c.predict('query.arff')
